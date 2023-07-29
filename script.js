c27e404abc099a8d94015ae5b696ba3e704704bd
Nishant Gurjar <118594124+nishantgurjar012@users.noreply.github.com>
2023-07-29 23:43:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-const library = [
-  {
-    author: "Bill Gates",
-    title: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    author: "Steve Jobs",
-    title: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+function numberOfBooksRead(library) {
+  // Filter the books array to get only the books with readingStatus as true (i.e., read books)
+  const readBooks = library.books.filter(book => book.readingStatus === true);
+
+  // Return the count of read books
+  return readBooks.length;
+}
+
+// Example library object
+const library = {
+  books: [
+    {
+      author: "J.K. Rowling",
+      title: "Harry Potter and the Sorcerer's Stone",
+      readingStatus: true
+    },
+    {
+      author: "George Orwell",
+      title: "1984",
+      readingStatus: false
+    },
+    {
+      author: "Jane Austen",
+      title: "Pride and Prejudice",
+      readingStatus: true
+    },
+    // Add more book objects here
+  ]
+};
+
+// Test the function
+const numberOfReadBooks = numberOfBooksRead(library);
+console.log(numberOfReadBooks); // Output: 2 (since there are two books with readingStatus set to true)
 
 const numberOfBooksRead = () => {
   // write your code here
